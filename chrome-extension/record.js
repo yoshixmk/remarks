fcf57e6e85b14e59b6ff522c1128aa66094f3c4f
yoshixmk <pirori67@gmail.com>
2021-09-19 15:41:50 +0900

    commented

--- chrome-extension/record.js
+++ chrome-extension/record.js
@@ -1,7 +1,7 @@
 const postData = async (data) => {
   const url = "https://remarks.deno.dev/remarks";
   const response = await fetch(url, {
-    method: "POST", // GET, POST, PUT, DELETE, etc.
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
     mode: "no-cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
     credentials: "same-origin", // include, *same-origin, omit
@@ -12,23 +12,19 @@ const postData = async (data) => {
     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify(data),
   });
-  return response.json();
+
+  // Chrome拡張は、ページごとのCORSの影響を受けるため自由にリクエストができないため、
+  // no-corsで送っている。その都合で、jsonを受け取ることができない
+  return response;
 };
 
 const buttons = Array.from(document.getElementsByTagName("button"));
 buttons.forEach((e) =>
   e.onclick = async () => {
-    try {
-      await postData({ text: e.value });
-      document.getElementById("success").classList.remove("is-hidden");
-    } catch (error) {
-      document.getElementById("error").classList.remove("is-hidden");
-      console.log(error);
-    } finally {
-      setTimeout(() => {
-        document.getElementById("success").classList.add("is-hidden");
-        document.getElementById("error").classList.add("is-hidden");
-      }, 3000);
-    }
+    await postData({ text: e.value });
+    document.getElementById("posted").classList.remove("is-hidden");
+    setTimeout(() => {
+      document.getElementById("posted").classList.add("is-hidden");
+    }, 2000);
   }
 );
